2de7eda72ea4e02b87cd18ad1ed2c32155b876ab
See Loo Jane <muigek34@yahoo.com.sg>
2015-09-15 20:53:01 +0800

    V0.1.1
    - Deleted unnecessary library imports

--- TextBuddy.java
+++ TextBuddy.java
@@ -29,13 +29,8 @@
 * @author See Loo Jane
 */
 
-import java.io.BufferedReader;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-
-import java.io.FileReader;
-import java.io.FileWriter;
 
 import java.io.IOException;
 
